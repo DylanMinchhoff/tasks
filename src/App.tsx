9d06655bda0f1c70722b397ab5cfcacd32ed4f0d
DylanMinchhoff <dylanminchhoff@gmail.com>
2024-02-07 12:33:11 -0500

    task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Container, Col, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,8 +8,27 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Dylan Minchhoff</p>
+            <h1>Dylan Minchhoff</h1>
             <p>Hello World!</p>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World!
+            </Button>
+            <ul>
+                <li>task 1</li>
+                <li>task 2</li>
+                <li>task 3</li>
+            </ul>
+            <img src="" alt="alt text img"></img>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="red-rectangle"></div>
+                    </Col>
+                    <Col>
+                        <div className="red-rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
