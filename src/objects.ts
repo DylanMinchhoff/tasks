60324593a1d0cfd215ebf7dbb9d57dff4ee8dd37
DylanMinchhoff <dylanminchhoff@gmail.com>
2024-02-26 22:59:33 -0500

    objects-with passing

--- src/objects.ts
+++ src/objects.ts
@@ -10,7 +10,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        body: "",
+        type: type,
+        options: [],
+        expected: "",
+        points: 1,
+        published: false
+    };
 }
 
 /**
@@ -21,7 +30,8 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const { expected } = question;
+    return expected.toLowerCase().trim() === answer.toLowerCase().trim();
 }
 
 /**
@@ -31,7 +41,9 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    return question.type == "multiple_choice_question"
+        ? question.options.indexOf(answer) != -1
+        : true;
 }
 
 /**
@@ -41,7 +53,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return `${question.id}: ${question.name.slice(0, 10)}`;
 }
 
 /**
@@ -62,7 +74,18 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    // prittier has an issue with the following line, and ends up fighting with eslint
+    // do not expand!
+    // eslint-disable-next-line prettier/prettier
+    const mark = `# ${question.name}\n${question.body}\n${
+        question.type === "multiple_choice_question"
+            ? question.options.reduce(
+                  (ps: string, cv: string) => ps + `- ${cv}\n`,
+                  ""
+              )
+            : ""
+    }`;
+    return mark.slice(0, mark.length - 1);
 }
 
 /**
@@ -70,7 +93,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, name: newName, options: [...question.options] };
 }
 
 /**
@@ -79,7 +102,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return {
+        ...question,
+        published: !question.published,
+        options: [...question.options]
+    };
 }
 
 /**
@@ -89,7 +116,13 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        id: id,
+        name: `Copy of ${oldQuestion.name}`,
+        published: false,
+        options: [...oldQuestion.options]
+    };
 }
 
 /**
@@ -100,7 +133,7 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return { ...question, options: [...question.options, newOption] };
 }
 
 /**
@@ -117,5 +150,12 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    return {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        options: [...contentQuestion.options],
+        published: false,
+        points: points
+    };
 }
