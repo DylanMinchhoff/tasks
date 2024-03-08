f70abed7d83b99b0c88371d96a9ad00672772ffc
DylanMinchhoff <dylanminchhoff@gmail.com>
2024-03-07 21:10:47 -0500

    task 10

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const RenderText = ({
+    name,
+    isStudent
+}: {
+    name: string;
+    isStudent: boolean;
+}): JSX.Element => {
+    //Your Name is a student
+    return (
+        <>
+            <p>{`${name} ${isStudent ? "is" : "is not"} a student`}</p>
+        </>
+    );
+};
 
 export function EditMode(): JSX.Element {
+    const [isEditMode, setEditMode] = useState<boolean>(false);
+    const [isStudent, setStudent] = useState<boolean>(true);
+    const [name, setName] = useState<string>("Your Name");
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="is-editMode"
+                label="Edit Mode"
+                checked={isEditMode}
+                onChange={() => setEditMode(!isEditMode)}
+            />
+            {isEditMode ? (
+                <>
+                    <Form.Check
+                        type="checkbox"
+                        id="is-student"
+                        label="Student"
+                        checked={isStudent}
+                        onChange={() => setStudent(!isStudent)}
+                    />
+                    <Form.Group controlId="formEditor">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </Form.Group>
+                </>
+            ) : (
+                <RenderText name={name} isStudent={isStudent} />
+            )}
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="favoriteColors">
+                <Form.Label>Please choose the correct answer</Form.Label>
+                <Form.Select
+                    value={choice}
+                    onChange={(e) => setChoice(e.target.value)}
+                >
+                    {options.map((o: string) => (
+                        <option key={`option-${o}`} value={o}>
+                            {o}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <p>{choice === expectedAnswer ? "✔️" : "❌"}</p>
         </div>
     );
 }
